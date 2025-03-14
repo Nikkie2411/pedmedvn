940439e4bbbcd0e6715858023c849ba93edda3c9
Nikkie2411 <163997288+Nikkie2411@users.noreply.github.com>
2025-03-14 16:21:56 +0700

    Add files via upload

--- utils/validation.js
+++ utils/validation.js
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 // Hàm kiểm tra email hợp lệ
 function isValidEmail(email) {
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
@@ -14,21 +13,4 @@ function isValidEmail(email) {
   module.exports = {
     isValidEmail,
     isValidPhone
-=======
-// Hàm kiểm tra email hợp lệ
-function isValidEmail(email) {
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return emailRegex.test(email);
-  }
-  
-  // Hàm kiểm tra số điện thoại hợp lệ (dành cho số Việt Nam bắt đầu bằng 03, 05, 07, 08, 09)
-  function isValidPhone(phone) {
-    const phoneRegex = /^(0[35789])[0-9]{8}$/;
-    return phoneRegex.test(phone);
-  }
-  
-  module.exports = {
-    isValidEmail,
-    isValidPhone
->>>>>>> Stashed changes
   };
