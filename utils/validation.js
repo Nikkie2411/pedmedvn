7823e8bbee4ced03c4a386a6e458afa5e4ef8ba4
Nikkie2411 <vietanh24@nch.gov.vn>
2025-03-14 13:28:40 +0700

    update

--- utils/validation.js
+++ utils/validation.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 // Hàm kiểm tra email hợp lệ
 function isValidEmail(email) {
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
@@ -13,4 +14,21 @@ function isValidEmail(email) {
   module.exports = {
     isValidEmail,
     isValidPhone
+=======
+// Hàm kiểm tra email hợp lệ
+function isValidEmail(email) {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+  }
+  
+  // Hàm kiểm tra số điện thoại hợp lệ (dành cho số Việt Nam bắt đầu bằng 03, 05, 07, 08, 09)
+  function isValidPhone(phone) {
+    const phoneRegex = /^(0[35789])[0-9]{8}$/;
+    return phoneRegex.test(phone);
+  }
+  
+  module.exports = {
+    isValidEmail,
+    isValidPhone
+>>>>>>> Stashed changes
   };
