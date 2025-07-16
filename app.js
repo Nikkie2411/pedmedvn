bfbfa20b0e8f53e4c830c3d4c24558395d2b0fd3
Viet Anh <nikkie.2411@gmail.com>
2025-07-16 23:45:14 +0700

    Remove therapy function

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ const authRoutes = require('./routes/auth');
 const drugRoutes = require('./routes/drugs');
 const passwordRoutes = require('./routes/password');
 const logger = require('./utils/logger');
-const therapyRouter = require('./routes/therapy');
 const cors = require('cors');
 
 const app = express();
@@ -49,7 +48,6 @@ async function startServer() {
   app.use('/api', authRoutes);
   app.use('/api', drugRoutes);
   app.use('/api', passwordRoutes);
-  app.use('/api', therapyRouter);
 
   const server = app.listen(PORT, '0.0.0.0', () => logger.info(`Server running on port ${PORT}`));
   app.locals.wss = setupWebSocket(server);
